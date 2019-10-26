d6562a4a47e7053e587f779043b42b5f871c2245
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-10-26 11:57:41 +0100

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 class Formatter {
   //add static methods here
 
-  static capitalize(word){
-    let array = word.split("");
-    array[0].toUpperCase();
-    return array;
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.substring(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-']+/g, '');
+
+  }
+
+  static titleize(string){
+    const permittedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let stringArray = string.split(" ");
+    
+    stringArray.forEach(word => {
+      if (permittedWords.find(word))  {
+        word = word.charAt(0).toUpperCase() + word.substring(1);
+      }
+    })
+
+    return stringArray.join(" ");
+
   }
 
 }
